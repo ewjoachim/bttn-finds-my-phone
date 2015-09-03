89224fab493408da781b16eb54822d314a6d1313
Joachim Jablon <ewjoachim@gmail.com>
2015-09-04 00:42:11 +0200

    First commit

---
+++ bttn/icloud_warn/views.py
@@ -0,0 +1,12 @@
+from pyicloud import PyiCloudService
+from django.views.generic import View
+from django import http
+
+
+class ICloudWarnView(View):
+
+    def post(self, request):
+        api = PyiCloudService(request.POST["email"], request.POST["password"])
+        d = [device for device in api.devices if device["name"] == request.POST["phone_name"]][0]
+        d.play_sound()
+        return http.HttpResponse("OK")
