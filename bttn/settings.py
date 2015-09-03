e5c0091179982cd763345d8eea8b1bc8d7a6dd4c
Joachim Jablon <ewjoachim@gmail.com>
2015-09-04 00:54:34 +0200

    no debug

--- bttn/settings.py
+++ bttn/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'gywo&mlr!^(z+0evkt*ht@w%t#g#z&+7_i87vy!2*e#v*hg$xt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
