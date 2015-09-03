e00720cded4126fe55b7a7be7ac6e0c1368a0f43
Joachim Jablon <ewjoachim@gmail.com>
2015-09-04 01:22:48 +0200

    finalize view

--- icloud_warn/views.py
+++ icloud_warn/views.py
@@ -1,12 +1,25 @@
 from pyicloud import PyiCloudService
-from django.views.generic import View
+from django.views.generic.edit import BaseFormView
 from django import http
+from django import forms
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
 
+class FindMyPhoneForm(forms.Form):
+    email = forms.CharField()
+    password = forms.CharField()
+    phone_name = forms.CharField()
 
-class FindMyPhoneView(View):
+class FindMyPhoneView(BaseFormView):
+    success_url = "find_my_phone"
+    form_class = FindMyPhoneForm
 
-    def post(self, request):
-        api = PyiCloudService(request.POST["email"], request.POST["password"])
-        d = [device for device in api.devices if device["name"] == request.POST["phone_name"]][0]
+    @method_decorator(csrf_exempt)
+    def dispatch(self, *args, **kwargs):
+        return super(FindMyPhoneView, self).dispatch(*args, **kwargs)
+
+    def form_valid(self, form):
+        api = PyiCloudService(form.cleaned_data["email"], form.cleaned_data["password"])
+        d = [device for device in api.devices if device["name"] == form.cleaned_data["phone_name"]][0]
         d.play_sound()
-        return http.HttpResponse("OK")
+        return super().form_valid(form)
